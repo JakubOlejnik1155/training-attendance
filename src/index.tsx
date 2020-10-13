441db8fb6d77d1ea925119335a6a9a0f81f989ae
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-10-13 11:06:27 +0200

    firs commit, project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import './styles/index.css';
+import App from './Components/App';
+import * as serviceWorker from './other/serviceWorker';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <Router  basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+ 
