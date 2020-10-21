590a4e95be2a762343a3f25f54c18d8e6a5b05d0
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-10-21 15:27:03 +0200

    add modals for adding and deleting competitors

--- src/App/Routes/Components/Competitor.jsx
+++ src/App/Routes/Components/Competitor.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
+import {Link} from 'react-router-dom'
+import { Button, Form, Modal } from 'react-bootstrap';
 import styled from 'styled-components';
 import deleteIcon from '../../../images/delete.svg';
+import {theme} from '../../../static/theme';
+import menuIcom from '../../../images/menu.svg';
+
 
 const Container = styled.div`
     width: 100%;
@@ -13,13 +18,13 @@ const Container = styled.div`
     justify-content: space-between;
     align-items: center;
 `;
-const Button = styled.button`
+const Btn = styled.button`
     padding: 5px;
     height: 100%;
     border: none;
     outline: none;
-    border-radius: 0 4px 4px 0;
     background-color: rgb(212,58,47);
+    border-radius: 0 4px 4px 0;
     transition: .3s all linear;
     &:hover{
         background-color: tomato;
@@ -34,24 +39,72 @@ const Attendance = styled.span`
     margin-left: auto;
     margin-right: 30px;
 `;
+const SeeMoreBtn = styled.button`
+    padding: 5px;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: ${theme.blue};
+    transition: .3s all linear;
+    &:hover{
+        background-color: lightblue;
+    }
+`;
 
 
 
+const Competitor = ({ name, surname, attendance}) => {
 
-const Competitor= ({ name, surname, attendance}) => {
+    const [show, setShow] = React.useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    //TODO: make deleting function
+    const handleDeleting = () =>{
+        console.log({ name, surname });
+        setShow(false);
+    }
     return (
-        <Container>
-            <span>
-                <strong>{name + ' '}</strong>
-                <i>{surname}</i>
-            </span>
-            <Attendance at={attendance}>
-                {attendance}%
-            </Attendance>
-            <Button>
-                <img src={deleteIcon} alt="bin" width="30" height="30"/>
-            </Button>
-        </Container>
+        <>
+            <Container>
+                <span>
+                    <strong>{name + ' '}</strong>
+                    <i>{surname}</i>
+                </span>
+                <Attendance at={attendance}>
+                    {attendance}%
+                </Attendance>
+                {/* //TODO: make this route for special user informations */}
+                <Link to={`/${name+surname}`}>
+                    <SeeMoreBtn>
+                        <img src={menuIcom} alt="menu" width="30" height="30" />
+                    </SeeMoreBtn>
+                </Link>
+                <Btn onClick={handleShow}>
+                    <img src={deleteIcon} alt="bin" width="30" height="30"/>
+                </Btn>
+            </Container>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete Competitor</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Are You sure that you want to delete:
+                    <span style={{color: 'red', fontWeight: 'bold'}}>{ ' '+name + ' '+ surname}</span>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="warning" onClick={handleClose}>
+                        Cancel
+                     </Button>
+                    <Button variant="danger" onClick={handleDeleting}>DELETE</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
      );
 }
 
--- src/App/Routes/Components/Competitors.tsx
+++ src/App/Routes/Components/Competitors.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Form, Modal } from 'react-bootstrap';
 import styled from 'styled-components';
 
 import { theme } from '../../../static/theme';
@@ -51,10 +51,37 @@ const data = [
     }
 ]
 
-
-
-
 const Competitors = () => {
+
+    const [state, setState] = React.useState({
+        name: '',
+        surname: '',
+        group: '',
+        date: ''
+    })
+    const [show, setShow] = React.useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const chaneHandler = (e:any) => {
+        if (e.target.name === "name")
+            setState({ ...state, name: e.target.value })
+        if (e.target.name === "surname")
+            setState({ ...state, surname: e.target.value })
+        if (e.target.name === "group")
+            setState({ ...state, group: e.target.value })
+        if (e.target.name === "date")
+            setState({ ...state, date: e.target.value })
+    }
+    //TODO: make adding competitor function
+    const AddHandler = (name: string, surname: string, group: string, date: string ) => {
+        console.log({name, surname, group, date});
+        setState({name: '',
+            surname: '',
+            group: '',
+            date: ''});
+        setShow(false);
+    }
+
     return(
         <NavTemplate>
             <Container>
@@ -68,11 +95,37 @@ const Competitors = () => {
                     />
                 ))}
                 <div className="w-100 d-flex justify-content-center align-items-center mt-4">
-                    <Button variant="primary">
+                    <Button variant="primary" onClick={handleShow}>
                         Add New One
                     </Button>
                 </div>
             </Container>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>New Competitor</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                     <Form.Label>Name</Form.Label>
+                    <Form.Control className="mb-2" type='text' name="name" value={state.name} onChange={chaneHandler}/>
+                     <Form.Label>Surname</Form.Label>
+                    <Form.Control className="mb-2" type='text' name="surname" value={state.surname} onChange={chaneHandler}/>
+                     <Form.Label>Group</Form.Label>
+                    <Form.Control className="mb-2" type='text' name="group" value={state.group} onChange={chaneHandler}/>
+                    <Form.Label>Ability date</Form.Label>
+                    <Form.Control className="mb-2" type='date' name="date" value={state.date} onChange={chaneHandler}/>
+                 </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="warning" onClick={handleClose}>
+                        Cancel
+                     </Button>
+                    <Button variant="success" onClick={() => AddHandler(state.name, state.surname, state.group, state.date)}>Add</Button>
+                </Modal.Footer>
+            </Modal>
 
         </NavTemplate>
     )
