7063d7ddc3e641febd5103b4bb07b6e4640669f6
Jakub Olejnik <jakub.k.olejnik@gmail.com>
2020-10-28 12:36:56 +0100

    Updates

---
+++ precache-manifest.664c4602cf28b83585757bf4ea2fbf22.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41541093a9fb9e9c306c00293cc05ee8",
+    "url": "/training-attendance/index.html"
+  },
+  {
+    "revision": "252e765e89adaa349e5c",
+    "url": "/training-attendance/static/css/2.b6e96c32.chunk.css"
+  },
+  {
+    "revision": "b96614e029d5c646a9fd",
+    "url": "/training-attendance/static/css/main.e8b6ea19.chunk.css"
+  },
+  {
+    "revision": "252e765e89adaa349e5c",
+    "url": "/training-attendance/static/js/2.0af264b0.chunk.js"
+  },
+  {
+    "revision": "6907fba2191d6d5ecb9717053a356074",
+    "url": "/training-attendance/static/js/2.0af264b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96614e029d5c646a9fd",
+    "url": "/training-attendance/static/js/main.643d0ff7.chunk.js"
+  },
+  {
+    "revision": "f6ac8106a8d1df2337f3",
+    "url": "/training-attendance/static/js/runtime-main.c66807f0.js"
+  },
+  {
+    "revision": "a9031342c3e8dca4d54430f1aef3ab3d",
+    "url": "/training-attendance/static/media/delete.a9031342.svg"
+  },
+  {
+    "revision": "1306dab72707c7ff75e6170dcddeef46",
+    "url": "/training-attendance/static/media/fitness.1306dab7.jpg"
+  },
+  {
+    "revision": "e6c1b22e6a2ab11f4b3d02ccc5540ad0",
+    "url": "/training-attendance/static/media/menu.e6c1b22e.svg"
+  },
+  {
+    "revision": "b97681d4b07b15d0a6f0e8f9818cac85",
+    "url": "/training-attendance/static/media/rowl.b97681d4.jpg"
+  },
+  {
+    "revision": "458890a65d47867c60d3b8163d509f2e",
+    "url": "/training-attendance/static/media/run.458890a6.jpg"
+  },
+  {
+    "revision": "4202a0dfd4c33afc97dd0801656bf24a",
+    "url": "/training-attendance/static/media/sailing.4202a0df.jpg"
+  },
+  {
+    "revision": "a24d637b97dedc7e8a087de732d66604",
+    "url": "/training-attendance/static/media/surf.a24d637b.jpg"
+  },
+  {
+    "revision": "cd8e9d31d79b6e5b13cf4f87ca822dfa",
+    "url": "/training-attendance/static/media/tenis.cd8e9d31.jpg"
+  },
+  {
+    "revision": "dcb8a836d501e68c863ad5ca91405d9f",
+    "url": "/training-attendance/static/media/yes.dcb8a836.svg"
+  }
+]);
